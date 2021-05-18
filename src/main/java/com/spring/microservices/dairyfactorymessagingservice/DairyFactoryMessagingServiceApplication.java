19050eed5884ccc5ff46da810bdf1e7748af3c4a
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-05-18 19:07:28 +0100

    Added initial project

---
+++ src/main/java/com/spring/microservices/dairyfactorymessagingservice/DairyFactoryMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.dairyfactorymessagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyFactoryMessagingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyFactoryMessagingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/microservices/dairyfactorymessagingservice/DairyFactoryMessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.dairyfactorymessagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyFactoryMessagingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
